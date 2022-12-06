eccdab3ba2f1f062c55a7e5f94937a8bd358a5c2
Mauricio García de Ceca <shinji.256@gmail.com>
2022-12-05 23:12:47 -0600

    Added IDs to every page to target elements in CSS. Added new texts.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -109,11 +109,9 @@ const Footer: FC<PropsWithChildren<Props>> = ({
                 <SocialButton label={'Twitter'} href={'https://twitter.com/holagig'}>
                   <FaTwitter />
                 </SocialButton>
-                {/*
                 <SocialButton label={'YouTube'} href={'https://youtube.com/holagig'}>
                   <FaYoutube />
                 </SocialButton>
-                */}
                 <SocialButton label={'Instagram'} href={'https://instagram.com/holagig'}>
                   <FaInstagram />
                 </SocialButton>

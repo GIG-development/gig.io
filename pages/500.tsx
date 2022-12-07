81eb3b82c787db5f27273c1fd7452a3f486c0b96
Mauricio García de Ceca <shinji.256@gmail.com>
2022-12-06 20:48:24 -0600

    UI changes to general layout of static content pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -2,6 +2,7 @@ import {
     Button,
     Heading,
     Link,
+    Text,
     Stack
 } from '@chakra-ui/react'
 import Head from '../components/Head'
@@ -17,8 +18,11 @@ const Custom500: NextPage = () => (
         />
         <Stack spacing={6} mb={20} align={'center'}>
             <Heading variant={'subtitle'} textAlign={'center'}>
-                Lo sentimos, hemos tenido un error. Si el error persiste, por favor repórtalo a <Link href='mailto:soporte@gig.io'>soporte@gig.io</Link> o en nuestra <Link href='/contacto'>página de contacto</Link>
+                Lo sentimos, hemos tenido un error
             </Heading>
+            <Text>
+                Si el error persiste, por favor repórtalo a <Link href='mailto:soporte@gig.io'>soporte@gig.io</Link> o en nuestra <Link href='/contacto'>página de contacto</Link>
+            </Text>
             <Link href='/'>
                 <Button>Volver al inicio</Button>
             </Link>

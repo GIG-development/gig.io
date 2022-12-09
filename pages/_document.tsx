214c961372f4126831b97024a4e38349f6288c37
Mauricio García de Ceca <shinji.256@gmail.com>
2022-12-09 16:23:24 -0600

    Added all SEO descriptions and titles metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,42 +9,6 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' href="favicon.png" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Source+Code+Pro:ital,wght@0,300;1,300&display=swap" rel="stylesheet" />
-          <script type="application/ld+json">
-            {
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "GIG Marketplace",
-              //"legalName" : "",
-              "url": "https://gig.io",
-              "logo": "https:/gig.io/logo.png",
-              "foundingDate": "2022",
-              "founders": [
-                {
-                  "@type": "Person",
-                  "name": "Jerónimo Vargas"
-                }
-              ],
-              "address": {
-                "@type": "PostalAddress",
-                //"streetAddress": "",
-                "addressLocality": "Mexico City",
-                "addressRegion": "Mexico City",
-                //"postalCode": "",
-                "addressCountry": "MX"
-              },
-              "contactPoint": {
-                "@type": "ContactPoint",
-                "contactType": "Soporte",
-                //"telephone": "[+]",
-                "email": "soporte@gig.io"
-              },
-              "sameAs": [ 
-                "https://www.instagram.com/holagig",
-                "https://www.twitter.com/holagig",
-                "https://www.youtube.com/holagig"
-              ]
-            }
-          </script>
         </Head>
         <body>
           <Main />

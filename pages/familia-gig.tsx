a7a3a26e39318f96743b00848eeda7f70e3b0302
Mauricio García de Ceca <shinji.256@gmail.com>
2022-12-18 22:47:41 -0600

    Created GIG Character component

--- pages/familia-gig.tsx
+++ pages/familia-gig.tsx
@@ -6,21 +6,12 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react'
 import Head from '../components/Head'
-import Character from 'components/Character/Character'
 import LargeLayout from '../layouts/large'
 import { NextPage } from 'next'
 import useTranslation from 'next-translate/useTranslation'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import NFTLali from '../public/img/animaciones/NFTLALI.json'
-import MAMADORIS from '../public/img/animaciones/MAMADORIS.json'
-import FLIPPER from '../public/img/animaciones/FLIPPER.json'
-import CREADORI from '../public/img/animaciones/CREADORI.json'
-import PAPACHE from '../public/img/animaciones/PAPACHE.json'
-import URI from '../public/img/animaciones/URI.json'
-import JEITER from '../public/img/animaciones/JEITER.json'
-import ESKERI from '../public/img/animaciones/ESKERI.json'
 
 const FamiliaGIG: NextPage = () => {
     const { t } = useTranslation('components')
@@ -28,7 +19,6 @@ const FamiliaGIG: NextPage = () => {
         arrows: true,
         dots: true,
         infinite: true,
-        centerMode: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
@@ -53,63 +43,18 @@ const FamiliaGIG: NextPage = () => {
                 </Stack>
                 <Box maxW={'720px'} margin={'0 auto 40px'}>
                     <Slider {...SliderSettings}>
-                        <Character
-                            name='NFTLali'
-                            animation={NFTLali}
-                            description={t('family.characters.nftlali.description')}
-                            powers={[t('family.characters.nftlali.power.power1'), t('family.characters.nftlali.power.power2'), t('family.characters.nftlali.power.power3')]}
-                            weaknesses={[t('family.characters.nftlali.weakness.weakness1')]}
-                        />
-                        <Character
-                            name='Mamá Doris'
-                            animation={MAMADORIS}
-                            description={t('family.characters.mamadoris.description')}
-                            powers={[t('family.characters.mamadoris.power.power1'), t('family.characters.mamadoris.power.power2'), t('family.characters.mamadoris.power.power3')]}
-                            weaknesses={[t('family.characters.mamadoris.weakness.weakness1'), t('family.characters.mamadoris.weakness.weakness2')]}
-                        />
-                        <Character
-                            name='Creadori Jiménez'
-                            animation={CREADORI}
-                            description={t('family.characters.creadori.description')}
-                            powers={[t('family.characters.creadori.power.power1'), t('family.characters.creadori.power.power2'), t('family.characters.creadori.power.power3')]}
-                            weaknesses={[t('family.characters.creadori.weakness.weakness1'), t('family.characters.creadori.weakness.weakness2')]}
-                        />
-                        <Character
-                            name='Flipper'
-                            animation={FLIPPER}
-                            description={t('family.characters.flipper.description')}
-                            description2={t('family.characters.flipper.description2')}
-                            powers={[t('family.characters.flipper.power.power1'), t('family.characters.flipper.power.power2'), t('family.characters.flipper.power.power3')]}
-                            weaknesses={[t('family.characters.flipper.weakness.weakness1'), t('family.characters.flipper.weakness.weakness2')]}
-                        />
-                        <Character
-                            name='Eskeri Popi'
-                            animation={ESKERI}
-                            description={t('family.characters.eskeri.description')}
-                            powers={[t('family.characters.eskeri.power.power1'), t('family.characters.eskeri.power.power2'), t('family.characters.eskeri.power.power3')]}
-                            weaknesses={[t('family.characters.eskeri.weakness.weakness1'), t('family.characters.eskeri.weakness.weakness2'), t('family.characters.eskeri.weakness.weakness3')]}
-                        />
-                        <Character
-                            name='Jeiter Popi'
-                            animation={JEITER}
-                            description={t('family.characters.jeiter.description')}
-                            powers={[t('family.characters.jeiter.power.power1'), t('family.characters.jeiter.power.power2'), t('family.characters.jeiter.power.power3')]}
-                            weaknesses={[t('family.characters.jeiter.weakness.weakness1'), t('family.characters.jeiter.weakness.weakness2')]}
-                        />
-                        <Character
-                            name='URI'
-                            animation={URI}
-                            description={t('family.characters.uri.description')}
-                            powers={[t('family.characters.uri.power.power1'), t('family.characters.uri.power.power2'), t('family.characters.uri.power.power3')]}
-                            weaknesses={[t('family.characters.uri.weakness.weakness1'), t('family.characters.uri.weakness.weakness2')]}
-                        />
-                        <Character
-                            name='Papá Che'
-                            animation={PAPACHE}
-                            description={t('family.characters.papache.description')}
-                            powers={[t('family.characters.papache.power.power1'), t('family.characters.papache.power.power2'), t('family.characters.papache.power.power3')]}
-                            weaknesses={[t('family.characters.papache.weakness.weakness1'), t('family.characters.papache.weakness.weakness2'), t('family.characters.papache.weakness.weakness3')]}
-                        />
+                        <div>
+                            <h1>NFTLali</h1>
+                            <p>{p('family.characters.nftlali.description')}</p>
+                        </div>
+                        <div>
+                            <h1>URI</h1>
+                            <p>{p('family.characters.nftlali.description')}</p>
+                        </div>
+                        <div>
+                            <h1>Flipper</h1>
+                            <p>{p('family.characters.nftlali.description')}</p>
+                        </div>
                     </Slider>
                 </Box>
             </LargeLayout>
